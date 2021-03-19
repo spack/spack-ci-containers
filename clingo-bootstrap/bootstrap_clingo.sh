5534250da26a01dd49f71c605db1c611cec34a79
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2021-03-19 16:24:27 +0100

    Update Dockerfile to bootstrap clingo

--- clingo-bootstrap/bootstrap_clingo.sh
+++ clingo-bootstrap/bootstrap_clingo.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-# Build clingo-bootstrap
+# Cycle over all the Python interpreters in manylinux1
+# and install the corresponding clingo-bootstrap binary
 for PYTHON in /opt/python/*/bin/python; do
-  # Bootstrap clingo
-  ${PYTHON} -m pip install --force-reinstall cmake
-  ${PYTHON} spack/bin/spack solve zlib
-  ${PYTHON} -m pip uninstall -y cmake
+  # Force reinstall a cmake for this Python interpreter
+  ${PYTHON} -m pip -qq install --force-reinstall cmake
+
+  # Install clingo using the current Python as an external
+  # The Python version will be output to stdout
+  ${PYTHON} spack/bin/spack python install_clingo.py
+
+  # Remove cmake for the next interpreter to work
+  ${PYTHON} -m pip -qq uninstall -y cmake
 done
+
+# Clean pip cache
+/opt/python/cp39-cp39/bin/python -m pip cache purge
+
+# Create a binary mirror
+/bin/bash create_binary_mirror.sh
 
